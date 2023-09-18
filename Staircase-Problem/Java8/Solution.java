9863c7cff7b51b468283f5cde9fd2b4b4078cff7
vishalgupta381 <vishalgupta381@gmail.com>
2023-09-18 13:21:19 +0530

    Create Solution.java

---
+++ Staircase-Problem/Java8/Solution.java
@@ -0,0 +1,48 @@
+// Problem Link - https://www.hackerrank.com/challenges/staircase/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    // Write your code here
+
+        for(int k = 1; k <= n; k++){
+            
+            for(int i = 1; i <= n-k; i++) {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j <= k; j++) {
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
